1a3dd25d88c4f887a32b20fbb0a2861b9afa475e
Bruno Vasconcelos <bvasconcelos710@gmail.com>
2023-10-06 22:31:11 -0300

    ajustes estilo

--- src/pages/MealsByIngredients.tsx
+++ src/pages/MealsByIngredients.tsx
@@ -54,7 +54,7 @@ const MealsByIngredient = () => {
                 />
                 <button
                     onClick={handleSearch}
-                    className="ml-2 px-4 py-2 bg-red-600 text-white rounded-full"
+                    className="ml-2 px-4 py-2 bg-red-600 text-white rounded-full hover:bg-red-900"
                 >
                     Pesquisar
                 </button>
